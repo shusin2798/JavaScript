ee015fe5f6d6d2f12322ab52f69bf495b0771ee3
shusin <shubham.singh@syncron.com>
2025-02-28 21:44:05 +0530

    Added mongodb.js, user and subscription model

--- subscription-tracker/app.js
+++ subscription-tracker/app.js
@@ -4,6 +4,7 @@ import { PORT } from './config/env.js';
 import userRouter from './routes/user.routes.js';
 import authRouter from './routes/auth.routes.js';
 import subscriptionRouter from './routes/subscription.routes.js';
+import connectToDatabase from './database/mongodb.js';
 
 const app = express();
 
@@ -15,8 +16,10 @@ app.get('/', (req, res) => {
     res.send('Welcome to the Subscription Tracker API!');
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
     console.log(`Subscription Tracker API is running on http://localhost:${PORT}`);
+
+    await connectToDatabase();
 });
 
 export default app;
