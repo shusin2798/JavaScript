db5897f88ae7170b68fccb3da9c7502949c2c51e
shusin <shubham.singh@syncron.com>
2025-03-15 06:38:22 +0530

    Implemented authentication on subscription-tracker

--- subscription-tracker/routes/auth.routes.js
+++ subscription-tracker/routes/auth.routes.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 
+import { signUp, signIn } from '../controllers/auth.controller.js';
+
 const authRouter = Router();
 
-authRouter.post('/sign-up', (req,res) => res.send({title: 'Sign Up'}));
+authRouter.post('/sign-up', signUp);
 
-authRouter.post('/sign-in', (req,res) => res.send({title: 'Sign In'}));
+authRouter.post('/sign-in', signIn);
 
-authRouter.post('/sign-out', (req,res) => res.send({title: 'Sign Out'}));
+//authRouter.post('/sign-out', signOut);
 
 export default authRouter;
