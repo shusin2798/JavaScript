3b93c8de2432e16dad7f4e085bc06dcb2acc512e
shusin <shubham.singh@syncron.com>
2025-03-16 07:16:19 +0530

    Implemented authorization for user routes

--- subscription-tracker/routes/user.routes.js
+++ subscription-tracker/routes/user.routes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
+import authorize from '../middlewares/auth.middleware.js';
+import { getUsers, getUser } from '../controllers/user.controller.js';
 
 const userRouter = Router();
 
-userRouter.get('/', (req, res) => res.send({title: 'GET all users'}));
+userRouter.get('/', getUsers);
 
-userRouter.get('/:id', (req, res) => res.send({title: 'GET user details'}));
+userRouter.get('/:id',authorize, getUser);
 
 userRouter.post('/', (req, res) => res.send({title: 'CREATE new user'}));
 
