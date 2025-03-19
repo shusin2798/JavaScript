871f771b46ad4ae2beedacf4e976270608171fa7
shusin <shubham.singh@syncron.com>
2025-03-19 08:34:30 +0530

    Implemented reminder workflows using Upstash

--- subscription-tracker/config/env.js
+++ subscription-tracker/config/env.js
@@ -3,9 +3,10 @@ import { config } from 'dotenv';
 config({path: `.env.${process.env.NODE_ENV || 'development'}.local`});
 
 export const { 
-    PORT, NODE_ENV, 
+    PORT, SERVER_URL, NODE_ENV, 
     DB_URI,
     JWT_SECRET,
     JWT_EXPIRES_IN,
-    ARCJET_ENV, ARCJET_KEY
+    ARCJET_ENV, ARCJET_KEY,
+    QSTASH_TOKEN, QSTASH_URL
 } = process.env
