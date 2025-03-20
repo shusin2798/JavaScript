a08890a7a070be2a48d4a88e6d653ba2eb6ce2d4
shusin <shubham.singh@syncron.com>
2025-03-20 11:28:07 +0530

    Implemented sending subscription reminder email

--- subscription-tracker/controllers/workflow.controller.js
+++ subscription-tracker/controllers/workflow.controller.js
@@ -3,6 +3,7 @@ import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 const { serve } = require("@upstash/workflow/express");
 import Subscription from '../models/subscription.model.js';
+import { sendReminderEmail } from '../utils/send-email.js'
 
 const REMINDERS = [7, 5, 2, 1]
 
@@ -26,9 +27,9 @@ export const sendReminders = serve(async (context) => {
       await sleepUntilReminder(context, `Reminder ${daysBefore} days before`, reminderDate);
     }
 
-    
-    await triggerReminder(context, `Reminder ${daysBefore} days before`);
-    
+    if (dayjs().isSame(reminderDate, 'day')) {
+      await triggerReminder(context, `${daysBefore} days before reminder`, subscription);
+    }
   }
 });
 
@@ -43,8 +44,14 @@ const sleepUntilReminder = async (context, label, date) => {
   await context.sleepUntil(label, date.toDate());
 }
 
-const triggerReminder = async (context, label) => {
-  return await context.run(label, () => {
+const triggerReminder = async (context, label, subscription) => {
+  return await context.run(label, async () => {
     console.log(`Triggering ${label} reminder`);
+
+    await sendReminderEmail({
+      to: subscription.user.email,
+      type: label,
+      subscription,
+    })
   })
 }
